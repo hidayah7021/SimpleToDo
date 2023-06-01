c32f4a8833fcd636ad2f9c20e804d2589c08b14f
hidayah7021 <22007021@myrp.edu.sg>
2023-06-01 14:33:36 +0800

    everything but enhancement and aaList.add(int,string)

--- app/src/main/java/sg/edu/rp/c346/id22007021/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22007021/simpletodo/MainActivity.java
@@ -3,12 +3,59 @@ package sg.edu.rp.c346.id22007021.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editInput;
+    EditText editPost;
+    TextView tvDisplay;
+    Button btnAdd;
+    Button btnClear;
+    ListView scheduleList;
+
+    ArrayList<String> addSchedule = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editInput = findViewById(R.id.editInput);
+        editPost = findViewById(R.id.editPost);
+        tvDisplay = findViewById(R.id.textDisplay);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnClear = findViewById(R.id.buttonClear);
+        scheduleList = findViewById(R.id.scheduleList);
+
+        addSchedule.add("Finish Worksheet");
+        addSchedule.add("Finish Reflection Journey");
+
+        ArrayAdapter aaList = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, addSchedule);
+        scheduleList.setAdapter(aaList);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sched = editInput.getText().toString();
+                int pos = Integer.parseInt(editPost.getText().toString());
+                aaList.notifyDataSetChanged();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editInput.getText().clear();
+            }
+        });
+
     }
 }
